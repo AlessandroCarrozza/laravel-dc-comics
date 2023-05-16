ea6622b82f4a9e1852424ed25851940b7044fb9c
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-05-16 22:21:55 +0200

    Progetto completato + bonus (attesa stilizzazione)

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -14,37 +14,37 @@ Crea un nuovo fumetto
 
     <div class="mb-3">
       <label for="title" class="form-label">Titolo</label>
-      <input type="email" class="form-control" id="title" aria-describedby="emailHelp">
+      <input type="text" class="form-control" id="title" name="title">
     </div>
 
     <div class="mb-3">
       <label for="description" class="form-label">Descrizione</label>
-      <textarea type="password" class="form-control" id="description"></textarea>
+      <textarea type="text" class="form-control" id="description" name="description"></textarea>
     </div>
 
     <div class="mb-3">
         <label for="thumb" class="form-label">Immagine</label>
-        <input type="password" class="form-control" id="thumb">
+        <input type="text" class="form-control" id="thumb" name="thumb">
     </div>
 
     <div class="mb-3">
         <label for="price" class="form-label">Prezzo</label>
-        <input type="password" class="form-control" id="price">
+        <input type="text" class="form-control" id="price" name="price">
     </div>
 
     <div class="mb-3">
         <label for="series" class="form-label">Serie</label>
-        <input type="password" class="form-control" id="series">
+        <input type="text" class="form-control" id="series" name="series">
     </div>
 
     <div class="mb-3">
         <label for="sale_date" class="form-label">Data</label>
-        <input type="password" class="form-control" id="sale_date">
+        <input type="text" class="form-control" id="sale_date" name="sale_date">
     </div>
 
     <div class="mb-3">
         <label for="type" class="form-label">Tipo</label>
-        <input type="password" class="form-control" id="type">
+        <input type="text" class="form-control" id="type" name="type">
     </div>
 
     <button type="submit" class="btn btn-primary">Aggiungi</button>
