5fb3afde6d3017dcbf01c296a48a4349817319fa
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-05-16 19:37:19 +0200

    lavorazione file show

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,12 @@
+@extends("layouts.app")
+
+@section("page-title")
+Fumetto: {{$comic->title}}
+@endsection
+
+@section("content")
+<img src="{{$comic->thumb}}" class="img-fluid" alt="{{$comic->title}}">
+<h1>{{$comic->title}}</h1>
+<p>{{$comic->description}}</p>
+<h4>${{$comic->price}}</h4>
+@endsection
