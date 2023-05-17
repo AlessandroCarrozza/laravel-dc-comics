087abddcb4c78baaafd05343b6ff55c8739e3446
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-05-17 19:16:31 +0200

    operazione delete completata

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -24,9 +24,15 @@
         <td>{{$comic->title}}</td>
         <td>${{$comic->price}}</td>
         <td>{{$comic->series}}</td>
-        <td>
-          <a href="{{route('comics.show', ['comic' => $comic->id])}}" type="button" class="btn btn-primary">Visualizza</a>
-          <a href="{{route('comics.edit', ['comic' => $comic->id])}}" type="button" class="btn btn-warning">Modifica</a>
+        <td class="d-flex">
+          <a href="{{route('comics.show', ['comic' => $comic->id])}}" type="button" class="btn btn-primary m-1">Visualizza</a>
+          <a href="{{route('comics.edit', ['comic' => $comic->id])}}" type="button" class="btn btn-warning m-1">Modifica</a>
+        
+          <form action="{{route("comics.destroy", ["comic" => $comic->id])}}" method="POST">
+            @csrf
+            @method("DELETE")
+            <button type="submit" class="btn btn-danger m-1">Elimina</button>
+          </form>
         </td>
 
       </tr>
