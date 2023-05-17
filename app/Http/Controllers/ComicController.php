ffc8b347ae7606570789eebd427f5cd864255ca9
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-05-17 16:31:00 +0200

    aggiunta del metodo fill

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -39,16 +39,19 @@ class ComicController extends Controller
         $data = $request->all();
 
         $newComic=new Comic();
-        $newComic->title = $data['title'];
+
+        $newComic->fill($data);
+
+        /*$newComic->title = $data['title'];
         $newComic->description = $data['description'];
         $newComic->thumb = $data['thumb'];
         $newComic->price = $data['price'];
         $newComic->series = $data['series'];
         $newComic->sale_date = $data['sale_date'];
-        $newComic->type = $data['type'];
+        $newComic->type = $data['type'];*/
         $newComic->save();
 
-        return redirect()->route('comics.show',['comic' => $newComic->id]);
+        return redirect()->route("comics.show",["comic" => $newComic->id]);
     }
 
     /**
