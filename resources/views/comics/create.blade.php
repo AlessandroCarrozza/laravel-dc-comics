0f8f065636efd3637cea472a0682307b633457c1
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-05-18 21:08:55 +0200

    validazioni aggiunte

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -15,37 +15,65 @@ Crea un nuovo fumetto
 
     <div class="mb-3">
       <label for="title" class="form-label">Titolo</label>
-      <input type="text" class="form-control" id="title" name="title">
+      <input type="text" class="form-control @error("title") is-invalid @enderror" id="title" name="title">
+
+      @error("title")
+      <div class="invalid-feedback">{{$message}}</div>
+      @enderror
     </div>
 
     <div class="mb-3">
       <label for="description" class="form-label">Descrizione</label>
-      <textarea type="text" class="form-control" id="description" name="description"></textarea>
+      <textarea type="text" class="form-control @error("description") is-invalid @enderror" id="description" name="description"></textarea>
+
+      @error("description")
+      <div class="invalid-feedback">{{$message}}</div>
+      @enderror
     </div>
 
     <div class="mb-3">
         <label for="thumb" class="form-label">Immagine</label>
-        <input type="text" class="form-control" id="thumb" name="thumb">
+        <input type="text" class="form-control @error("thumb") is-invalid @enderror" id="thumb" name="thumb">
+
+        @error("thumb")
+        <div class="invalid-feedback">{{$message}}</div>
+        @enderror
     </div>
 
     <div class="mb-3">
         <label for="price" class="form-label">Prezzo</label>
-        <input type="text" class="form-control" id="price" name="price">
+        <input type="text" class="form-control @error("price") is-invalid @enderror" id="price" name="price">
+
+        @error("price")
+        <div class="invalid-feedback">{{$message}}</div>
+        @enderror
     </div>
 
     <div class="mb-3">
         <label for="series" class="form-label">Serie</label>
-        <input type="text" class="form-control" id="series" name="series">
+        <input type="text" class="form-control @error("series") is-invalid @enderror" id="series" name="series">
+
+        @error("series")
+        <div class="invalid-feedback">{{$message}}</div>
+        @enderror
     </div>
 
     <div class="mb-3">
         <label for="sale_date" class="form-label">Data</label>
-        <input placeholder="0000-00-00" type="text" class="form-control" id="sale_date" name="sale_date">
+        <input placeholder="0000-00-00" type="text" class="form-control @error("sale_date") is-invalid @enderror" id="sale_date" name="sale_date">
+
+        @error("sale_date")
+        <div class="invalid-feedback">{{$message}}</div>
+        @enderror
     </div>
 
     <div class="mb-3">
         <label for="type" class="form-label">Tipo</label>
-        <input type="text" class="form-control" id="type" name="type">
+        <input type="text" class="form-control @error("type") is-invalid @enderror" id="type" name="type">
+
+        @error("type")
+        <div class="invalid-feedback">{{$message}}</div>
+        @enderror
     </div>
 
     <button type="submit" class="btn btn-primary">Aggiungi</button>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,18 @@
 
     <main>
         <div class="container">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
         @yield("content")
+        
         </div>
     </main>
 
