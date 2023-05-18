7ed0b2449bbec5666c0533c47664fcc334f6b4dd
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-05-18 21:17:27 +0200

    aggiunta funzione old nel form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -15,7 +15,7 @@ Crea un nuovo fumetto
 
     <div class="mb-3">
       <label for="title" class="form-label">Titolo</label>
-      <input type="text" class="form-control @error("title") is-invalid @enderror" id="title" name="title">
+      <input type="text" class="form-control @error("title") is-invalid @enderror" id="title" name="title" value="{{old("title")}}">
 
       @error("title")
       <div class="invalid-feedback">{{$message}}</div>
@@ -24,7 +24,7 @@ Crea un nuovo fumetto
 
     <div class="mb-3">
       <label for="description" class="form-label">Descrizione</label>
-      <textarea type="text" class="form-control @error("description") is-invalid @enderror" id="description" name="description"></textarea>
+      <textarea type="text" class="form-control @error("description") is-invalid @enderror" id="description" name="description">{{old("title")}}</textarea>
 
       @error("description")
       <div class="invalid-feedback">{{$message}}</div>
@@ -33,7 +33,7 @@ Crea un nuovo fumetto
 
     <div class="mb-3">
         <label for="thumb" class="form-label">Immagine</label>
-        <input type="text" class="form-control @error("thumb") is-invalid @enderror" id="thumb" name="thumb">
+        <input type="text" class="form-control @error("thumb") is-invalid @enderror" id="thumb" name="thumb" value="{{old("thumb")}}">
 
         @error("thumb")
         <div class="invalid-feedback">{{$message}}</div>
@@ -42,7 +42,7 @@ Crea un nuovo fumetto
 
     <div class="mb-3">
         <label for="price" class="form-label">Prezzo</label>
-        <input type="text" class="form-control @error("price") is-invalid @enderror" id="price" name="price">
+        <input type="text" class="form-control @error("price") is-invalid @enderror" id="price" name="price" value="{{old("price")}}">
 
         @error("price")
         <div class="invalid-feedback">{{$message}}</div>
@@ -51,7 +51,7 @@ Crea un nuovo fumetto
 
     <div class="mb-3">
         <label for="series" class="form-label">Serie</label>
-        <input type="text" class="form-control @error("series") is-invalid @enderror" id="series" name="series">
+        <input type="text" class="form-control @error("series") is-invalid @enderror" id="series" name="series" value="{{old("series")}}">
 
         @error("series")
         <div class="invalid-feedback">{{$message}}</div>
@@ -60,7 +60,7 @@ Crea un nuovo fumetto
 
     <div class="mb-3">
         <label for="sale_date" class="form-label">Data</label>
-        <input placeholder="0000-00-00" type="text" class="form-control @error("sale_date") is-invalid @enderror" id="sale_date" name="sale_date">
+        <input placeholder="0000-00-00" type="text" class="form-control @error("sale_date") is-invalid @enderror" id="sale_date" name="sale_date" value="{{old("sale_date")}}">
 
         @error("sale_date")
         <div class="invalid-feedback">{{$message}}</div>
@@ -69,7 +69,7 @@ Crea un nuovo fumetto
 
     <div class="mb-3">
         <label for="type" class="form-label">Tipo</label>
-        <input type="text" class="form-control @error("type") is-invalid @enderror" id="type" name="type">
+        <input type="text" class="form-control @error("type") is-invalid @enderror" id="type" name="type" value="{{old("type")}}">
 
         @error("type")
         <div class="invalid-feedback">{{$message}}</div>
